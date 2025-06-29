c94e00f4a31a568cfd53ccf62af11bd771432ff5
Noah Goertzen <noah.goertzen@gmail.com>
2025-06-28 21:48:35 -0500

    dwa

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("Welcome to my website!");
-console.log("Testing 12");
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+console.log("hello world");
